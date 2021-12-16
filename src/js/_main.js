2c0aeae0abaaeae2aa1dae732569c9b099eb0893
Vladislav <arkhipovvladislav@mai.ru>
2021-12-16 12:19:47 +0300

    add new files

--- src/js/_main.js
+++ src/js/_main.js
@@ -11,7 +11,7 @@ import './modules/polyfills/closest';
 import './modules/Menu';
 import './modules/Mask';
 import './modules/Swap';
-import './modules/FormHiding';
+// import './modules/FormHiding';
 import './modules/Player';
 import './modules/Tab';
 import './modules/Drawer';
@@ -21,3 +21,18 @@ import './modules/Scroll';
 
 // sliders
 import './modules/sliders/banner';
+
+const $modalCard = document.querySelector('.js_aside__form');
+const $modalContainer = document.querySelector('.mobile-form__wrapper');
+const $modalAside = document.querySelector('.aside__wrapper');
+const mobileWidth = window.matchMedia('(max-width: 992px)');
+
+window.addEventListener('resize', function() {
+  if (mobileWidth.matches) {
+    $modalCard.remove();
+    $modalContainer.insertAdjacentElement('afterbegin', $modalCard);
+  } else {
+    $modalCard.remove();
+    $modalAside.insertAdjacentElement('afterbegin', $modalCard);
+  }
+});
